35b5e91d94844d511fc2d3fb84c608ab6177332c
LamHieu27 <daoty5376919@gmail.com>
2026-05-03 23:54:30 +0700

    update UI

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
       className="text-center py-10 border-t border-gray-800 mt-20"
     >
       <p className="text-gray-400">
-        © {new Date().getFullYear()} Hiếu. Built with Next.js
+        © {new Date().getFullYear()} LamHieu27
       </p>
     </footer>
   );
